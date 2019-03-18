28e4af489b61537df265e68700e064e01206876b
Matt <matt@mspacecreative.com>
2019-03-18 16:45:06 -0300

    c

--- wp-content/themes/tsm/includes/hero_slider.php
+++ wp-content/themes/tsm/includes/hero_slider.php
@@ -14,10 +14,10 @@
 						<?php endif; ?>
 						<?php
 						$image = get_sub_field('image_content');
-						$size = 'medium';
-						if ( $image ) {
-						echo wp_get_attachment_image( $image, $size );
-						} ?>
+						$size = 'large';
+						if ( $image ) : ?>
+						<div class="hero-text-img"><?php echo wp_get_attachment_image( $image, $size ); ?></div>
+						<?php endif; ?>
 						<!-- /BLURB TEXT -->
 						
 						<?php if( have_rows('hero_button') ): 
